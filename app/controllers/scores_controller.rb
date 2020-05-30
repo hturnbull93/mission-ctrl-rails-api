be03c98eec4e9db7635a838de079fdfaaf817f17
Harry Turnbull <hturnbull@live.co.uk>
2020-05-30 13:53:04 +0100

    G: post to /scores

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,4 +3,17 @@ class ScoresController < ApplicationController
     @scores = Score.all.order(score: :desc)
     render json: @scores
   end
+
+  def create
+    @score = Score.new(score_params)
+    if @score.save
+      render json: @score
+    end
+  end
+
+  private
+
+  def score_params
+    params.permit(:name, :score)
+  end
 end
