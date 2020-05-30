cf58301a1f951a37dd851971cd432ea91640f85b
Harry Turnbull <hturnbull@live.co.uk>
2020-05-30 12:07:11 +0100

    R: get all scores sorted by score

---
+++ spec/controllers/scores_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe ScoresController, type: :controller do
+
+  it "GET /scores returns all scores, sorted by score " do
+    Score.create(name: "Dave", score: 100)
+    Score.create(name: "Pete", score: 80)
+    Score.create(name: "Jim", score: 90)
+    get :index
+
+    json = JSON.parse(response.body)
+
+    expect(response).to have_http_status(200)
+
+    expect(json[0]['score']).to eq 100
+    expect(json[1]['score']).to eq 90
+    expect(json[2]['score']).to eq 80
+  end
+
+
+end
