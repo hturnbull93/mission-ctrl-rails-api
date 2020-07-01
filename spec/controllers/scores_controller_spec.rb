d11cd72125e0cc92a677aa2b0c92e50d825c4bde
Harry Turnbull <hturnbull@live.co.uk>
2020-07-01 10:08:16 +0100

    Lint

--- spec/controllers/scores_controller_spec.rb
+++ spec/controllers/scores_controller_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ScoresController, type: :controller do
-
-  it "GET /scores returns all scores, sorted by score " do
-    Score.create(name: "Dave", score: 100)
-    Score.create(name: "Pete", score: 80)
-    Score.create(name: "Jim", score: 90)
+  it 'GET /scores returns all scores, sorted by score ' do
+    Score.create(name: 'Dave', score: 100)
+    Score.create(name: 'Pete', score: 80)
+    Score.create(name: 'Jim', score: 90)
     get :index
 
     json = JSON.parse(response.body)
@@ -17,25 +18,23 @@ RSpec.describe ScoresController, type: :controller do
     expect(json[2]['score']).to eq 80
   end
 
-
   it 'POST /scores submits a score' do
     post :create, params: {
-      name: "Dave",
+      name: 'Dave',
       score: 100
     }
 
     json = JSON.parse(response.body)
 
     expect(json['score']).to eq 100
-    expect(json['name']).to eq "Dave"
+    expect(json['name']).to eq 'Dave'
   end
 
   it 'POST /scores with bad data returns 500' do
     post :create, params: {
-      wrong: "uhoh"
+      wrong: 'uhoh'
     }
 
     expect(response).to have_http_status(:unprocessable_entity)
-end
-
+  end
 end
